28454a639acf3bee0db0bbaddf4e51f11b0df4cf
congtien169 <congtien169@gmail.com>
2020-05-18 08:11:17 +0700

    add require

--- cay_quyet_dinh_np.py
+++ cay_quyet_dinh_np.py
@@ -59,35 +59,20 @@ for i in range(row_target[0]):
     target_so[i] = assign_number_tagert(target_chu[i])
 print(target_so)
 
-"""
-feature_so = [
-    ['1', '3', '3', '7'],
-    ['5', '2', '4', '6'],
-    ['1', '2', '4', '6'],
-    ['5', '4', '4', '6'],
-    ['1', '4', '4', '7'],
-    ['3', '2', '3', '7'],
-    ['3', '3', '3', '6'],
-    ['5', '2', '2', '7']
-]
-tagget_so = ['0', '1', '1', '0', '0', '0', '0', '1']
-"""
 
+# tranning
 my_tree.fit(X=feature_so, y=target_so)
 
-"""
-np_quy_uoc_feature = np.array([
-    ['nhe', 'thap', 'tb', 'cao', 'nang', 'it', 'nhieu'],
-    [1, 2, 3, 4, 5, 6, 7]
-])
-"""
+# data test
+# TODO convert data test from char to int
 feature_test_chu_1 = [['nhe', 'cao', 'tb', 'it']]
 feature_test_chu_2 = [['nhe', 'cao', 'tb', 'nhieu']]
 
 feature_test_so_1 = [['1', '2', '5', '7']]
 feature_test_so_2 = [['1', '4', '4', '7']]
 
+# run test
 result = my_tree.predict(X=feature_test_so_1)
-print(type(result))
 print(result)
+print('he so hoi quy : ', my_tree.score(feature_test_so_1,result))
 
